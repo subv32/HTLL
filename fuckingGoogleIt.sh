694830e8e3a794267511275bd6eeb7f605782205
subv32 <subv32@national.shitposting.agency>
2015-07-02 05:34:01 -0900

    Update fuckingGoogleIt.sh

--- fuckingGoogleIt.sh
+++ fuckingGoogleIt.sh
@@ -3,18 +3,34 @@ lastAsshole=""
 shopt -s nocasematch
 while :
 do
+        sleep 5
+        number=$(( ( RANDOM % 1000 ) + 1 ))
+
         currentAsshole=$(python getShoutbox.py | cut -d":" -f3- | tr -d "'")
-        currentAsshole=$(echo -e "$currentAsshole" | grep -i "how to\|how do" | tail -1)
-        if [[ "$lastAsshole" == "$currentAsshole" ]]; then
+        googleIt=$(echo -e "$currentAsshole" | grep -i "how to\|how do" | tail -1)
+        
+        #Every once in a long while we will just scramble their words and reply.. cause why the fuck not
+        if [ "$number" -gt 992 ]; then
+                yodaIAm=$(echo -e "$currentAsshole" | tr " " "\n" | sort -R | tr "\n" " ")
+                python post.py subv32 password 1 "$yodaIAm"
                 continue
         fi
+        
 
-        if [[ "$currentAsshole" == *"how to"* ]] || [[ "$currentAsshole" == *"how do"* ]]; then
-                googleStuff=$(echo -e "$currentAsshole" | tr " " "+")
-                googleStuff="https://www.google.com/?gws_rd=ssl#q=$googleStuff"
-                python post.py subv32 password 1 "$googleStuff"
-                lastAsshole=$currentAsshole
+        ##Only sometimes will we tell them to fucking google it.. Slightly less than ~50% of the time
+        if [ "$number" -lt 500 ]; then
+                continue
+        else
+                if [[ "$googleIt" == *"how to"* ]] || [[ "$googleItAsshole" == *"how do"* ]]; then
+                        #If the last message is the current message then we already told them to fucking google it
+                        if [[ "$lastAsshole" == "$googleIt" ]]; then
+                                continue
+                        fi
+                
+                        googleStuff=$(echo -e "$googleIt" | tr " " "+")
+                        googleStuff="https://www.google.com/?gws_rd=ssl#q=$googleStuff"
+                        python post.py subv32 password 1 "$googleStuff"
+                        lastAsshole=$googleIt
+                fi
         fi
-        sleep 5
 done
-
