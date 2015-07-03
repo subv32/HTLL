a24ac581b5fc2081fae35a84b02b4df978f1586b
subv32 <subv32@national.shitposting.agency>
2015-07-02 15:43:23 -0900

    clearly I was more drunk than i thought when i wrote this

--- fuckingGoogleIt.sh
+++ fuckingGoogleIt.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
-lastAsshole=""
+lastShout=""
+randomFire=0
 shopt -s nocasematch
 while :
 do
         sleep 5
         number=$(( ( RANDOM % 1000 ) + 1 ))
 
-        currentAsshole=$(python getShoutbox.py | cut -d":" -f3- | tr -d "'")
-        googleIt=$(echo -e "$currentAsshole" | grep -i "how to\|how do" | tail -1)
-        
-        #Every once in a long while we will just scramble their words and reply.. cause why the fuck not
-        if [ "$number" -gt 992 ]; then
-                yodaIAm=$(echo -e "$currentAsshole" | tr " " "\n" | sort -R | tr "\n" " ")
-                python post.py subv32 password 1 "$yodaIAm"
+        currentShout=$(python getShoutbox.py | cut -d":" -f3- | tr -d "'")
+
+        googleIt=$(echo -e "$currentShout" | grep -i "how to\|how do" | tail -1)
+
+        #If the last message is the current message then we already told them to fucki$
+        if [[ "$lastShout" == "$currentShout" ]] || [[ $randomFire -eq 1 ]]; then
                 continue
         fi
-        
+
+        lastShout=$currentShout 
+        #Every once in a long while we will just scramble their words and reply.. cause why the fuck not
+        if [ "$number" -gt 990 ]; then
+                randomSortedShout=$(echo -e "$currentShout" | tail -1 | tr " " "\n" | sort -R | tr "\n" " ")
+                python post.py subv32 password 1 "$randomSortedShout"
+                randomFire=1
+                continue
+        fi
+
+        randomFire=0
+     
 
         ##Only sometimes will we tell them to fucking google it.. Slightly less than ~50% of the time
         if [ "$number" -lt 500 ]; then
                 continue
         else
-                if [[ "$googleIt" == *"how to"* ]] || [[ "$googleItAsshole" == *"how do"* ]]; then
-                        #If the last message is the current message then we already told them to fucking google it
-                        if [[ "$lastAsshole" == "$googleIt" ]]; then
-                                continue
-                        fi
-                        #Remove text "how to" or "how do"
+                if [[ "$googleIt" == *"how to"* ]] || [[ "$googleIt" == *"how do"* ]]; then
                         googleIt=$(echo -e "$googleIt" | sed s/'how to'// | sed s/'how do'//)
-                        
-                        #Replace spaces with pluses so it will make a valid google search link
                         googleStuff=$(echo -e "$googleIt" | tr " " "+")
-                        
-                        #Make search link
                         googleStuff="https://www.google.com/?gws_rd=ssl#q=$googleStuff"
                         python post.py subv32 password 1 "$googleStuff"
-                        lastAsshole=$googleIt
                 fi
         fi
 done
