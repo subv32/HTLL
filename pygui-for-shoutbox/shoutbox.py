465f9521803e4a671d8fdfad6717d1b3c9937abc
subv32 <subv32@national.shitposting.agency>
2017-01-08 20:41:37 -0500

    bug fix
    should fix the bug where some chat messages would show up in strange places

--- pygui-for-shoutbox/shoutbox.py
+++ pygui-for-shoutbox/shoutbox.py
@@ -3,6 +3,7 @@ import tkinter.scrolledtext as tkst
 from tkinter import font
 import os
 import time
+import re
 class Application(tk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
@@ -39,25 +40,20 @@ class Application(tk.Frame):
 			self.shoutbox.insert(tk.INSERT, "\n")
 			a=a+1
 		self.shoutbox.see(tk.END)
-		self.oldChat = self.chat
-		self.oldUsers = self.users
+		self.oldBox = self.getCurrentBox("all")
 		self.printBox()
-	def print_contents(self, event):
-		print(self.userinput.get())
 
-		command="bash  htll-functions.sh --function postToChat --message \"" + str(self.userinput.get()) + "\""
-		print(command)
+	def print_contents(self, event):
+
+		command="bash htll-login.sh --function postToChatApi --message \"" + str(self.userinput.get()) + "\""
+		#print(command)
 		os.system(command)
 		self.shoutinput.delete('0','end')
 	def printBox(self):
-		self.newChat=self.getCurrentBox("chat")
-		self.newUsers=self.getCurrentBox("user")
-		if self.newUsers != self.oldUsers:
-			strNewUsers=str(self.newUsers)
-			strNewChat=str(self.newChat)
-
-			strOldUsers=str(self.oldUsers)
-			strOldChat=str(self.oldChat)
+		self.newBox=self.getCurrentBox("all")
+		if self.newBox != self.oldBox:
+			strNewChat=str(self.newBox)
+			strOldChat=str(self.oldBox)
 
 			strNewChatL=[]
 			for i in strNewChat.split("\n"):
@@ -71,55 +67,23 @@ class Application(tk.Frame):
 				changesChat.append(i)
 				#print("adding " + i + " to changesChat")
 			changesChat = format("\n".join(changesChat[1:]))
-
-			numberOfChatChanges=len(changesChat.split('\n'))
-
-			strNewUsersL=[]
-			for i in strNewUsers.split("\n"):
-				strNewUsersL.append(i)
-			strNewUsersL.reverse()
-
-			print(strNewUsersL)
-			changesUsers=[]
-			print(numberOfChatChanges)
-			totalUsers=0
-			for i in strNewUsersL:
-				if totalUsers > numberOfChatChanges:
-					break
-				totalUsers=totalUsers+1
-				changesUsers.append(i)
-
-			print(changesUsers)
-			#Convert changeUsers and changesChat back into strs
-		#	print(changesUsers)
-			changesUsers = format("\n".join(changesUsers[1:]))
-			print(changesUsers)
-
-			#changesChat=strNewChat.split(strOldChat.splitlines()[-1], 1)[1]
-			#changesUsers=strNewUsers.split(strOldUsers.splitlines()[-1], 1)[1]
-#			print(changesUsers)
 #			print(changesChat)
+#			numberOfChatChanges=len(changesChat.split('\n'))
 
-			#self.shoutbox.insert(tk.INSERT, changes.rstrip())
-			#self.shoutbox.see(tk.END)
-			a=0
-			for i in changesUsers.split('\n'):
-				if i == "":
-					continue
-				self.shoutbox.insert(tk.INSERT, i + ": ", 'boldFont')
-				try:
-					self.shoutbox.insert(tk.INSERT, changesChat.splitlines()[a], 'normalFont')
-				except:
-					pass
-				self.shoutbox.insert(tk.INSERT, "\n")
-				a=a+1
+			username=re.sub("[^[[A-Z]\|[a-z]]\d]", "", re.search("^[^:]*", changesChat).group(0))
+			message=re.search(":.*$", changesChat).group(0)
+
+			self.shoutbox.insert(tk.INSERT, username, 'boldFont')
+			self.shoutbox.insert(tk.INSERT, message, 'normalFont')
+			self.shoutbox.insert(tk.INSERT, "\n")
+			#	a=a+1
 		self.shoutbox.see(tk.END)
-		self.oldUsers=self.newUsers
-		self.oldChat=self.newChat
+		self.oldBox=self.newBox
 		self.after(2000, self.printBox)
+		#self.printBox
 	def getCurrentBox(self, getparam):
 		self.currentBox= tk.StringVar()
-		self.currentBox.set(os.popen("bash htll-functions.sh --function getShoutyUsingApi --get " + str(getparam)).read())
+		self.currentBox.set(os.popen("bash htll-login.sh --function getShoutyUsingApi --get " + str(getparam)).read())
 		return self.currentBox.get()
 
 root = tk.Tk()
